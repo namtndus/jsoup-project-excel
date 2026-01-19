fcf3adef4b296adea43391de3893c515b2020fed
namtndus <rnrmf0721@gmail.com>
2026-01-19 22:53:46 +0900

    setting: jsoup server file create

---
+++ jsoup-sever/src/main/java/com/example/jsoup_sever/JsoupSeverApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsoup_sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoupSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsoupSeverApplication.class, args);
+	}
+
+}
---
+++ jsoup-sever/src/test/java/com/example/jsoup_sever/JsoupSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsoup_sever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsoupSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
