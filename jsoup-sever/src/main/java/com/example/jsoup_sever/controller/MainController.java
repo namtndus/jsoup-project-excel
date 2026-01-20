34749129c6b6397fdae18355ca70ccb4eacb079f
namtndus <rnrmf0721@gmail.com>
2026-01-20 22:32:25 +0900

    feat: server first sever function create

---
+++ jsoup-sever/src/main/java/com/example/jsoup_sever/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.jsoup_sever.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}
